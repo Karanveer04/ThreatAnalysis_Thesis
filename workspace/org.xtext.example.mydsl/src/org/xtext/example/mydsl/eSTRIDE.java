82d6ff7f9d39e9d10a1bad690f4c513401927af8
Andrius Sakalas <a.sakalas2@gmail.com>
2019-05-06 13:47:42 +0200

    User input addition + User input path

--- workspace/org.xtext.example.mydsl/src/org/xtext/example/mydsl/eSTRIDE.java
+++ workspace/org.xtext.example.mydsl/src/org/xtext/example/mydsl/eSTRIDE.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.Scanner;
 import java.util.Set;
 import com.google.common.collect.HashMultimap;
 import eDFDFlowTracking.Flow;
@@ -15,63 +16,72 @@ public class eSTRIDE {
 
 		BundlingAndFolding bundle = new BundlingAndFolding();
 		eSTRIDEMap estride = new eSTRIDEMap();
-
-		File inputModel = new File(
-				"C:\\Users\\Karan\\Git Projects\\Thesis\\ArchitecturalThreatAnalysis\\runtime\\xText\\src\\eDFD.mydsl");
-
+		Scanner choice = new Scanner(System.in);
+		
+		System.out.println("Input path to the .mydsl file, including the file name + extension name");
+		String path = choice.next();
+		File inputModel = new File(path);
 		Set<Flow> suggestionList = new HashSet<>();
 		HashMultimap<String, String> FlowBundlingTable = HashMultimap.create();
-		;
-
-//------Data Bundling Process---------------------------      
+		
+		// ------Data Bundling Process---------------------------
 		suggestionList = bundle.FlowBundle(inputModel);
-//------eSTRIDE----------------------------------- 
+		// ------eSTRIDE-----------------------------------
 		HashMultimap<String, String> eSTRIDETable = estride.mapToeSTRIDE(inputModel);
-//------Process Folding----------------------------------------------
+		// ------Process Folding----------------------------------------------
 		ArrayList<ArrayList<Process>> finalPair = new ArrayList<ArrayList<Process>>();
 		finalPair = bundle.ProcessFold(inputModel);
-//----PRINTING----
-
-		// -------------------------Process Folding
-		// Suggestions------------------------------------------------
-		System.out.println(String.format("%25s %23s", "Process Folding Suggestions", "|"));
-		System.out.println(String.format("%s", "---------------------------------------------------"));
-		for (int i = 0; i < finalPair.size(); i++) {
-			System.out.println(String.format("%-25s %25s", finalPair.get(i).get(i).getName(),
-					finalPair.get(i).get(i + 1).getName()));
-		}
-		System.out.println();
-		System.out.println();
-
-		// -------------------------Flow Bundling
-		// Suggestions------------------------------------------------
-		System.out.println("Data Bundling Suggestions: ");
-		System.out.println(String.format("%25s %25s %70s %50s", "Entity", "|", "Flows", "|"));
-		System.out.println(String.format("%s",
-				"-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------"));
-		for (Flow flow : suggestionList) {
-			for (Flow matchFlow : suggestionList) {
-				if (flow.getSource().getName().equals(matchFlow.getSource().getName())) {
-					FlowBundlingTable.put(flow.getSource().getName(), flow.getName());
-					FlowBundlingTable.put(matchFlow.getSource().getName(), flow.getName());
+		// ----PRINTING----
+		while (true) {
+			System.out.println("0 Suggestion List, 1 eSTRIDE, 2 exit");
+			int n = choice.nextInt();
+			if (n == 0) {
+				// -------------------------Process Folding
+				// Suggestions------------------------------------------------
+				System.out.println(String.format("%25s %23s", "Process Folding Suggestions", "|"));
+				System.out.println(String.format("%s", "---------------------------------------------------"));
+				for (int i = 0; i < finalPair.size(); i++) {
+					System.out.println(String.format("%-25s %25s", finalPair.get(i).get(i).getName(),
+							finalPair.get(i).get(i + 1).getName()));
 				}
+				System.out.println();
+				System.out.println();
+
+				// -------------------------Flow Bundling
+				// Suggestions------------------------------------------------
+				System.out.println("Data Bundling Suggestions: ");
+				System.out.println(String.format("%25s %25s %70s %50s", "Entity", "|", "Flows", "|"));
+				System.out.println(String.format("%s",
+						"-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------"));
+				for (Flow flow : suggestionList) {
+					for (Flow matchFlow : suggestionList) {
+						if (flow.getSource().getName().equals(matchFlow.getSource().getName())) {
+							FlowBundlingTable.put(flow.getSource().getName(), flow.getName());
+							FlowBundlingTable.put(matchFlow.getSource().getName(), flow.getName());
+						}
+					}
+				}
+				for (String key : FlowBundlingTable.keySet()) {
+					System.out
+							.println(String.format("%-25s %25s %-70s %50s", key, "|", FlowBundlingTable.get(key), "|"));
+				}
+				System.out.println("");
+				System.out.println("");
+			} else if (n == 1) {
+//-------------------------------eSTRIDE Threat Table-------------------------------------------
+				System.out.println("eSTRIDE Threat Table: ");
+				System.out.println();
+				System.out.println(String.format("%25s %25s %70s %50s", "Entity", "|", "STRIDE Category", "|"));
+				System.out.println(String.format("%s",
+						"-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------"));
+
+				for (String key : eSTRIDETable.keySet()) {
+					System.out.println(String.format("%-25s %25s %-70s %50s", key, "|", eSTRIDETable.get(key), "|"));
+				}
+			} else {
+				break;
 			}
 		}
-		for (String key : FlowBundlingTable.keySet()) {
-			System.out.println(String.format("%-25s %25s %-70s %50s", key, "|", FlowBundlingTable.get(key), "|"));
-		}
-		System.out.println("");
-		System.out.println("");
-
-//-------------------------------eSTRIDE Threat Table-------------------------------------------
-		System.out.println("eSTRIDE Threat Table: ");
-		System.out.println();
-		System.out.println(String.format("%25s %25s %70s %50s", "Entity", "|", "STRIDE Category", "|"));
-		System.out.println(String.format("%s",
-				"-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------"));
-
-		for (String key : eSTRIDETable.keySet()) {
-			System.out.println(String.format("%-25s %25s %-70s %50s", key, "|", eSTRIDETable.get(key), "|"));
-		}
+		choice.close();
 	}
 }
